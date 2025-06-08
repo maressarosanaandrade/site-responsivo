77af950322fa59951814ccbbd86c794c2690042b
Maressa Rosana Andrade <maressarandrade@gmail.com>
2025-06-08 13:04:42 -0300

    git commit -m "Atualização Final" git push origin main

--- contato.js
+++ contato.js
@@ -1,3 +1,18 @@
 document.getElementById("btn-voltar").addEventListener("click", function () {
     window.location.href="index.html";
 });
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const msgSucesso = document.createElement('div');
+    msgSucesso.classList.add('mensagem-sucesso');
+    msgSucesso.textContent = 'Mensagem enviada com sucesso! Obrigado por entrar em contato.';
+
+    document.body.appendChild(msgSucesso);
+
+    setTimeout(() => {
+        msgSucesso.remove();
+    }, 3000);
+    this.reset();
+});
